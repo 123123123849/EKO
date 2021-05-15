4c27a5eb7e9fb0331c4c2b5e5af99aa9a9eb7af5
123123123849 <84223931+123123123849@users.noreply.github.com>
2021-05-15 11:53:13 +0300

    Add files via upload

---
+++ set/set/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set
+{
+    class Class1
+    {
+        private double a;
+        private double b;
+        private double S;
+
+
+        public Class1(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public double met()
+        {
+            S = 0.5 * a * b;
+            S = Math.Sin(S);
+            return S;
+        }
+    }
+}
---
+++ set/set/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Введите длины сторон треугольника ");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            Class1 Tr = new Class1(a, b);
+            Console.WriteLine("Площадь : " +Tr.met());
+            Console.ReadKey();
+        }
+    }
+}
